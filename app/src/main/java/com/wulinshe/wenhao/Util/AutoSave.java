20bb40d89ef1b51a277681bbe09e65a77b2caaf0
bingzi <dz6168@gmail.com>
2017-05-07 22:30:22 +0800

    Initial commit

---
+++ app/src/main/java/com/wulinshe/wenhao/Util/AutoSave.java
@@ -0,0 +1,15 @@
+package com.wulinshe.wenhao.Util;
+
+import android.widget.EditText;
+
+/**
+ * Created by Administrator on 2017/4/25.
+ */
+
+public class AutoSave {
+    public  void  Save(){
+
+        EditText editText;
+
+    }
+}
